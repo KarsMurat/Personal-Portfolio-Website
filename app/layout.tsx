1ceaf0fd202074c5430b4146435642325460b404
Murat Kars <mk.muratkars@gmail.com>
2023-01-16 23:07:34 +0300

    layout and switcher edited

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="grid h-screen grid-cols-1 justify-items-center bg-zinc-900 p-8">
-        <div className="relative flex w-8/12">
+      <body className="grid h-screen grid-cols-1 justify-items-center bg-zinc-900 p-4">
+        <div className="relative -mr-72 flex w-8/12">
           <div className="fixed left-0 flex h-screen">
             <div className="m-auto">
               <PersonalCard />
             </div>
           </div>
-          <div className="m-4 grid flex-1 grid-cols-1 place-content-between justify-items-center space-y-4 rounded-lg bg-zinc-800 p-4 shadow-sm shadow-black">
+          <div className="m-4 grid flex-1 grid-cols-1 place-content-between justify-items-center space-y-4 rounded-lg bg-zinc-800 p-8 shadow-sm shadow-black">
             <div className="absolute -top-4 -right-4">
               <Navbar />
             </div>
--- components/Switcher.tsx
+++ components/Switcher.tsx
@@ -1,6 +1,6 @@
 export default function Switcher() {
   return (
-    <div className="absolute bottom-0 right-0 animate-pulse cursor-pointer">
+    <div className="absolute -bottom-8 -right-8 m-4 animate-pulse cursor-pointer p-4">
       <div
         className="h-0 w-0 rounded-tr-3xl rounded-bl-3xl
       border-l-[75px] border-b-[75px] border-r-[0px]
