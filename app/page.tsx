bc71c904435b9a0136be43d1f64cc284aa0d1d87
Murat Kars <mk.muratkars@gmail.com>
2023-01-16 23:47:22 +0300

    homepage text changed with lorem

--- app/page.tsx
+++ app/page.tsx
@@ -3,188 +3,68 @@ export default function HomePage() {
     <div>
       <div>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Aspernatur provident reiciendis impedit laudantium porro cum
+          sunt temporibus eos quis nemo natus autem delectus eligendi
+          a modi sapiente deserunt, amet illo!
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Distinctio voluptatibus eaque autem voluptate. Libero nihil
+          accusamus quasi eveniet minus eius, facere veritatis minima
+          maiores eos nesciunt iste quas perspiciatis quidem,
+          voluptatibus provident temporibus officiis, recusandae
+          itaque quae ut?
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Ipsum inventore sapiente beatae ullam blanditiis eos,
+          pariatur iste! Perspiciatis harum, cupiditate id molestiae
+          iure expedita quos dolor blanditiis quaerat? Illum, fuga.
+          Minima aliquam aperiam possimus ut modi natus consectetur.
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
-        </h1>
-      </div>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Quae nisi at voluptatibus ad repudiandae eos, harum alias
+          odio nobis quos blanditiis libero nulla atque nihil
+          explicabo. Optio vitae nobis, nostrum dolore facere sint eos
+          ea perferendis aliquam aliquid.
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Soluta reprehenderit neque error dolorem iure optio!
+          Corporis quia omnis veniam laboriosam vero aperiam eius nam
+          beatae! Nisi aliquam incidunt ad illum quasi neque labore
+          quibusdam veniam quas corrupti? Natus.
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Vitae temporibus sit facere pariatur, nobis nihil. Saepe
+          architecto culpa aliquam ipsa vero nam necessitatibus
+          similique totam ab? Neque consequuntur facere voluptates
+          sapiente facilis praesentium deserunt repellendus eius quos
+          hic!
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
-        </h1>
-      </div>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Expedita voluptate recusandae sapiente error perspiciatis
+          suscipit quod, vero iste aut delectus, laudantium quibusdam
+          explicabo ipsam perferendis voluptas eveniet, tempore
+          quaerat sed natus. Eius dolor minima, laudantium eligendi ea
+          impedit.
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Deserunt, nemo! Atque, odio consequatur! Harum commodi
+          voluptates accusantium accusamus. Sapiente ullam accusamus
+          odio ut iure perferendis doloribus fugit adipisci
+          distinctio, quasi quam perspiciatis dolor. Velit laboriosam
+          expedita perferendis. Consectetur.
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Quo, velit odit placeat, ratione nesciunt at enim, dolores
+          quisquam autem dolorem eaque cumque mollitia sit ab ducimus
+          aliquid? Quaerat beatae pariatur repellat expedita eum,
+          numquam sed obcaecati? Ipsam, unde.
         </h1>
         <h1 className="text-3xl font-bold underline">
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage Homepage Homepage Homepage Homepage
-          Homepage Homepage
+          Amet ut corporis est explicabo vero quasi molestias a porro
+          minus fuga quia reprehenderit ratione mollitia, eum beatae
+          maxime blanditiis soluta sunt excepturi! Odio ea voluptate
+          dolores quas libero quis.
         </h1>
       </div>
     </div>
