a8c83a4e4e8dfc54fc33d22a264af5c8c34ddc1d
Murat Kars <mk.muratkars@gmail.com>
2023-01-18 02:36:28 +0300

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,23 +15,23 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="grid h-screen grid-cols-1 justify-items-center bg-zinc-900 p-4">
-        <div className="relative -mr-72 flex w-8/12">
-          <div className="fixed left-0 flex h-screen">
+
+      <body className="h-screen w-screen justify-items-center bg-zinc-900">
+        <div className="sticky flex h-full justify-center -space-x-8">
+          <div className="flex h-screen">
             <div className="m-auto">
               <PersonalCard />
             </div>
           </div>
-          <div className="m-4 grid flex-1 grid-cols-1 place-content-between justify-items-center space-y-4 rounded-lg border-2 border-zinc-800 bg-zinc-900 p-8 shadow-xl shadow-black">
-            <div className="absolute -top-4 -right-4">
-              <Navbar />
-            </div>
-            {children}
-            <div>
+          <div className="relative w-8/12 overflow-y-scroll">
+            <div className="m-8 grid min-h-[93%] flex-1 grid-cols-1 place-content-between justify-items-center space-y-4 rounded-lg border-2 border-zinc-800 bg-zinc-900 p-8 shadow-xl shadow-black">
+              {children}
               <Footer />
             </div>
+            <div className="absolute -top-0 -right-0">
+              <Navbar />
+            </div>
           </div>
-          <Switcher />
         </div>
       </body>
     </html>
