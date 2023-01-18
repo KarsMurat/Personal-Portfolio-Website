b3b446197d322835039777bfc686045938a57006
Murat Kars <mk.muratkars@gmail.com>
2023-01-19 01:05:03 +0300

    update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,14 @@
 export default function Footer() {
-  return <div className="mx-auto text-zinc-400">Copyright</div>;
+  function Divide() {
+    return <div className="h-1 w-full rounded-full bg-zinc-800" />;
+  }
+
+  return (
+    <div className="w-full text-zinc-400">
+      <Divide />
+      <div className="mt-6 grid justify-center text-zinc-400">
+        Copyright
+      </div>
+    </div>
+  );
 }
